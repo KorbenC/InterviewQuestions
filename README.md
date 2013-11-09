ec5b2323a66dedd9f4e9f14df0b32abb8d037b0d
Korben <korbenc@gmail.com>
2013-11-08 23:50:30 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+#Interview Questions
+I'm gonna place some interview questions and answers here. Probably focus on C/C++
+
+Author: Korben Carreno (http://www.korbencarreno.com)  
+Version: 1.0 2013-11-08)   
+Copyright 2013, Korben Carreno
---
+++ Strings/reversestr.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<string.h>
+ 
+void reverse(char*);
+ 
+int main() 
+{
+   char str[100];
+ 
+   printf("Enter a string\n");
+   gets(str);
+ 
+   reverse(str);
+ 
+   printf("Reverse of entered string is \"%s\".\n", str);
+   getch();
+   return 0;
+}
+ 
+void reverse(char *str) 
+{
+   int length, c;
+   char *begin, *end, temp;
+ 
+   length = strlen(str);
+ 
+   begin = str;
+   end = str;
+ 
+   for ( c = 0 ; c < ( length - 1 ) ; c++ )
+      end++;
+      
+   for ( c = 0 ; c < length/2 ; c++ ) 
+   {        
+      temp = *end;
+      *end = *begin;
+      *begin = temp;
+ 
+      begin++;
+      end--;
+   }
+}
