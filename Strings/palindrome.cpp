892077d2fbac90f6539ebb475efdcd785b1ec8ed
Korben <korbenc@gmail.com>
2013-11-12 01:12:16 -0500

    palindrome commit

---
+++ Strings/palindrome.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    char str[80];
+    bool palindrome = true;
+    
+    cout << "Enter a word: ";
+    cin >> str;
+    
+    int length = strlen(str);
+    for(int i = 0; i < length; i++)
+    {
+          str[i] = tolower(str[i]);
+    }
+    
+    for(int i = 0; i < length/2; i++)
+    {
+            if(str[i] != str[length - i -1])
+            {
+                      palindrome = false;
+                      break;
+            }
+    }
+    
+    if(palindrome == true)
+    {
+                  cout << str << " is a palindrome." << endl;
+    }
+    else
+    {
+                  cout << str << " is not a palindrome." << endl;
+    }
+    
+    return 0;
+}
