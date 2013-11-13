8b9edb2f2de9e73f5aa09204e016b8debf427f1c
Korben <korbenc@gmail.com>
2013-11-13 18:34:38 -0500

    remove char from strings commit

---
+++ Strings/removeCharFromString.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string str = "test";
+    char ch[1];
+    int length = (str.length());
+    bool found = true;
+    do{
+    cout << "The word is: " << str << endl;
+    cout << "Enter a letter: ";
+    cin >> ch;
+    
+  
+    for(int j = 0; j < length; j++)
+    {
+            if(str.find(ch) != std::string::npos)
+            {
+                      found = true;
+                      for(int i = 0; i < strlen(ch); ++i)
+                      {
+                              str.erase(std::remove(str.begin(), str.end(), ch[i]), str.end());
+                      }
+                      break;
+            }
+            if(str.find(ch) == std::string::npos)
+            {
+                            cout << endl << ch << " not found in " << str << endl;
+                            found = false;
+                            break;
+            }
+    }
+    }while(!found);
+    
+    cout << str << endl;
+    return 0;
+}
